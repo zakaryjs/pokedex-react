06173fc9d8c6d2e1bc71c2a54d16be224db98fab
Zakary <119669322+zakaryjs@users.noreply.github.com>
2023-12-30 01:16:57 +0800

    use external library for interfaces, add type and loading icons,  update styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,30 @@
 import { useEffect } from 'react';
 import './App.css';
 import { useGetRandomPokemon } from './hooks/useGetRandomPokemon';
+import pokeball from './images/pokeball.gif'
+import Button from 'react-bootstrap/Button';
 
 function App() {
 
-  let { pokemon, getPokemon } = useGetRandomPokemon()
+  let { pokemon, getPokemon, type1, type2, loading } = useGetRandomPokemon()
 
   useEffect(() => {
     getPokemon()
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
-  useEffect(() => {
-    console.log(pokemon)
-  }, [pokemon])
-
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Pokedex</h1>
-        <div className='pokemon-box'>
-          {pokemon.name && <h1>{pokemon.name}</h1>}
-          {pokemon.sprites && <img src={pokemon.sprites?.other?.home?.front_default} alt='pokemon' height={200} width={200}/>}
+        <h1 id='header-text'>Pokedex</h1>
+        <div className={pokemon?.name ? 'pokemon-box' : ''}>
+          {!loading && pokemon?.name && <h1>{pokemon.name}</h1>}
+          {!loading && pokemon?.sprites && <img src={pokemon.sprites?.front_default} alt='pokemon' height={200} width={200}/>}
+          {!loading && type1 && <img src={`https://raw.githubusercontent.com/msikma/pokesprite/master/misc/types/go/${type1}.png`} alt='type badge'/>}
+          {!loading && type2 && <img src={`https://raw.githubusercontent.com/msikma/pokesprite/master/misc/types/go/${type2}.png`} alt='type badge'/>}
         </div>
+        {loading && <img src={pokeball} alt='pokeball' height={100} width={100}/>}
+        {!loading && <Button variant='danger' onClick={() => {getPokemon()}}>Get New Random Pokemon</Button>}
       </header>
     </div>
   );
