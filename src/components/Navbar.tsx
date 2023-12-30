0bac719fcabca1d2155f7d3244741d4af24c802f
Zakary <119669322+zakaryjs@users.noreply.github.com>
2023-12-30 16:45:41 +0800

    centred navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,12 +3,12 @@ import '../styles/Navbar.css'
 
 export default function Navbar () {
     return (
-        <>
+        <div className="centred">
             <NavLink to="/" className={({isActive}) => isActive ? 'styledSelected' : 'styled'}>Home</NavLink>
             <NavLink to="/region" className={({isActive}) => isActive ? 'styledSelected' : 'styled'}>Region</NavLink>
             <NavLink to="/search" className={({isActive}) => isActive ? 'styledSelected' : 'styled'}>S</NavLink>
             <NavLink to="/favourites" className={({isActive}) => isActive ? 'styledSelected' : 'styled'}>Favourites</NavLink>
             <NavLink to="/settings" className={({isActive}) => isActive ? 'styledSelected' : 'styled'}>Settings</NavLink>
-        </>
+        </div>
     )
 }
