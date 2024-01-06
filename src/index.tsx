d72e4186779088214f7dd9295fc09f3bbf0c4b7a
Zakary <119669322+zakaryjs@users.noreply.github.com>
2024-01-06 15:36:56 +0800

    fix: region images now load consistently with pokemon on region pages (#11)
    * fix: images now load with pokemon on region pages

--- src/index.tsx
+++ src/index.tsx
@@ -12,6 +12,15 @@ import Favourites from './pages/Favourites';
 import Settings from './pages/Settings';
 import RegionPage from './pages/regions/RegionPage';
 import Pokemon from './pages/Pokemon';
+import kanto from './images/kanto.png'
+import johto from './images/johto.png'
+import hoenn from './images/hoenn.png'
+import sinnoh from './images/sinnoh.png'
+import unova from './images/unova.png'
+import kalos from './images/kalos.png'
+import alola from './images/alola.png'
+import galar from './images/galar.png'
+import paldea from './images/paldea.png'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -25,15 +34,15 @@ root.render(
         <Route path='/search' element={<Search />} />
         <Route path='/favourites' element={<Favourites />} />
         <Route path='/settings' element={<Settings />} />
-        <Route path='/region/kanto' element={<RegionPage name="Kanto" limit={151} offset={0} link="https://archives.bulbagarden.net/media/upload/thumb/2/25/LGPE_Kanto_Map.png/320px-LGPE_Kanto_Map.png" />} />
-        <Route path='/region/johto' element={<RegionPage name="Johto" limit={100} offset={151} link="https://archives.bulbagarden.net/media/upload/thumb/6/64/JohtoMap.png/320px-JohtoMap.png" />} />
-        <Route path='/region/hoenn' element={<RegionPage name="Hoenn" limit={135} offset={251} link="https://archives.bulbagarden.net/media/upload/thumb/8/85/Hoenn_ORAS.png/320px-Hoenn_ORAS.png" />} />
-        <Route path='/region/sinnoh' element={<RegionPage name="Sinnoh" limit={107} offset={386} link="https://archives.bulbagarden.net/media/upload/thumb/0/08/Sinnoh_BDSP_artwork.png/320px-Sinnoh_BDSP_artwork.png" />} />
-        <Route path='/region/unova' element={<RegionPage name="Unova" limit={156} offset={493} link="https://archives.bulbagarden.net/media/upload/thumb/f/fc/Unova_B2W2_alt.png/320px-Unova_B2W2_alt.png" />} />
-        <Route path='/region/kalos' element={<RegionPage name="Kalos" limit={72} offset={649} link="https://archives.bulbagarden.net/media/upload/thumb/8/8a/Kalos_alt.png/320px-Kalos_alt.png" />} />
-        <Route path='/region/alola' element={<RegionPage name="Alola" limit={88} offset={721} link="https://archives.bulbagarden.net/media/upload/thumb/0/0b/Alola_USUM_artwork.png/320px-Alola_USUM_artwork.png" />} />
-        <Route path='/region/galar' element={<RegionPage name="Galar" limit={96} offset={809} link="https://archives.bulbagarden.net/media/upload/thumb/c/ce/Galar_artwork.png/170px-Galar_artwork.png" />} />
-        <Route path='/region/paldea' element={<RegionPage name="Paldea" limit={120} offset={905} link="https://archives.bulbagarden.net/media/upload/thumb/f/fd/Paldea_artwork.png/320px-Paldea_artwork.png" />} />
+        <Route path='/region/kanto' element={<RegionPage name="Kanto" limit={151} offset={0} link={kanto} />} />
+        <Route path='/region/johto' element={<RegionPage name="Johto" limit={100} offset={151} link={johto} />} />
+        <Route path='/region/hoenn' element={<RegionPage name="Hoenn" limit={135} offset={251} link={hoenn} />} />
+        <Route path='/region/sinnoh' element={<RegionPage name="Sinnoh" limit={107} offset={386} link={sinnoh} />} />
+        <Route path='/region/unova' element={<RegionPage name="Unova" limit={156} offset={493} link={unova} />} />
+        <Route path='/region/kalos' element={<RegionPage name="Kalos" limit={72} offset={649} link={kalos} />} />
+        <Route path='/region/alola' element={<RegionPage name="Alola" limit={88} offset={721} link={alola} />} />
+        <Route path='/region/galar' element={<RegionPage name="Galar" limit={96} offset={809} link={galar} />} />
+        <Route path='/region/paldea' element={<RegionPage name="Paldea" limit={120} offset={905} link={paldea} />} />
         <Route path='/pokemon/:name' element={<Pokemon />} />
       </Routes>
       <Navbar />
