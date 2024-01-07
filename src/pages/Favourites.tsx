7d26b7ee440b7013f34b593f0a04d0156ab9047b
Zakary <119669322+zakaryjs@users.noreply.github.com>
2024-01-07 14:38:22 +0800

    feat: added options to settings page

--- src/pages/Favourites.tsx
+++ src/pages/Favourites.tsx
@@ -5,7 +5,6 @@ import { useNavigate } from "react-router-dom";
 export default function Favourites () {
 
     const [favouritePokemon, setFavouritePokemon] = useState<PokeAPI.Pokemon[]>([])
-    const [visible, setVisible] = useState<boolean>(false)
     const navigate = useNavigate()
 
     function getFavourites() {
@@ -14,18 +13,10 @@ export default function Favourites () {
         )
 
         setFavouritePokemon(favouriteArray)
-        if (favouriteArray.length > 0) {
-            setVisible(true)
-        }
 
         favouriteArray.forEach((pokemon: PokeAPI.Pokemon) => {
             console.log(pokemon)
         });
-    }
-
-    function clearLocalStorage () {
-        localStorage.removeItem('favourites')
-        window.location.reload()
     }
 
     useEffect(() => {
@@ -36,7 +27,6 @@ export default function Favourites () {
         <div className="App">
             <header className="App-header">
                 <h1>Favourites</h1>
-                {visible && <button onClick={() => {clearLocalStorage()}}>Clear</button>}
                 {favouritePokemon!.map((pokemon: PokeAPI.Pokemon) => (
                     <div key={pokemon.name} onClick={() => {navigate(`/pokemon/${pokemon.name}`)}}>
                         <h1>{pokemon.name}</h1>
