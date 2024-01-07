3269b5bd5a5565c53927f02853bf16881785e0ec
Zakary <119669322+zakaryjs@users.noreply.github.com>
2024-01-07 14:38:44 +0800

    feat: added options to settings page (#14)

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,7 +1,21 @@
-
+import Button from 'react-bootstrap/Button';
 
 export default function Settings () {
+
+    const repoUrl = 'https://github.com/zakaryjs/pokedex-react'
+
+    function clearLocalStorage () {
+        localStorage.removeItem('favourites')
+        window.location.reload()
+    }
+
     return (
-        <h1>Settings</h1>
+        <div className="App">
+            <header className="App-header">
+                <h1>Settings</h1>
+                <Button className="button-margin" variant="info" onClick={() => {clearLocalStorage()}}>Clear Favourites</Button>
+                <Button className="button-margin" variant="info" onClick={() => {window.open(repoUrl)}}>View the GitHub Repo</Button>
+            </header>
+        </div>
     )
 }
